a98ba5c19763ea5c922ba36e24740696a31e9247
Valet-V0ult-de-Fur1e <82310399+Valet-V0ult-de-Fur1e@users.noreply.github.com>
2024-10-24 12:58:18 +0300

    update lab2

---
+++ Patterns of generation/LocationGenerator/Location.cs
@@ -0,0 +1,28 @@
+﻿namespace OOP.LocationGenerator
+{
+    internal class Location
+    {
+        private protected string floorMaterial;
+        private protected string name;
+        private protected string musicalTheme;
+
+        public void setFloorMaterial(string floorMaterial)
+        {
+            this.floorMaterial = floorMaterial;
+        }
+
+        public void setName(string name)
+        {
+            this.name = name;
+        }
+
+        public void setMusicalTheme(string musicalTheme)
+        {
+            this.musicalTheme = musicalTheme;
+        }
+        public string toString()
+        {
+            return string.Format("Location(name={0}, floorMaterial={1}, musicalTheme={2})', this.getName(), this.getId())", this.name, this.floorMaterial, this.musicalTheme);
+        }
+    }
+}
---
+++ Patterns of generation/LocationGenerator/OutsideBuilder/OutsideBuilder.cs
@@ -0,0 +1,34 @@
+﻿using OOP.Spawner.Interfaces;
+
+namespace OOP.LocationGenerator.OutsideBuilder
+{
+    internal class OutsideBuilder : LocationBuilder
+    {
+        private protected Location location;
+
+        public OutsideBuilder()
+        {
+            this.location = new Location();
+        }
+
+        public void buildFloorMaterial()
+        {
+            location.setFloorMaterial("земля");
+        }
+
+        public void buildName()
+        {
+            location.setName("поле");
+        }
+
+        public void buildSetMusicalTheme()
+        {
+            location.setMusicalTheme("По полям синий трактор едет к нам.mp3");
+        }
+
+        public Location getResult()
+        {
+            return location;
+        }
+    }
+}
